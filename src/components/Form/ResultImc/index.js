d5f36280d291f593ea0b89edc04dad930543e29f
Alan <alan.ghb@gmail.com>
2022-09-14 21:15:36 -0300

    carga

---
+++ src/components/Form/ResultImc/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, Share, TouchableOpacity } from 'react-native';
+
+import styles from './style';
+
+export default function ResultImc(props) {
+
+    const onShare = async () => {
+        const result = await Share.share({
+            message: "Meu IMC hoje é: " + props.resultImc,
+        })
+    }
+
+    return (
+        <View style={styles.contextImc}>
+            <View style={styles.boxShareButton}>
+                <Text style={styles.information}>{props.messageResultImc}</Text>
+                <Text style={styles.numberImc}>{props.resultImc}</Text>
+                <TouchableOpacity onPress={onShare} style={styles.shared}>
+                    <Text style={styles.sharedText}>Compartilhar</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
---
+++ src/components/Form/style.js
@@ -0,0 +1,71 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    formContext: {
+    //       width: "100%",
+    //    height: "100%",
+    //    justifyContent: "space-around",
+        backgroundColor: "#ffffff",
+        alignItems: "center",
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        paddingTop: 30,
+    },
+    form: {
+        width: "100%",
+    },
+    formLabel: {
+        color: "#000000",
+        fontSize: 18,
+        paddingLeft: 20,
+    },
+    input: {
+        width: "90%",
+        borderRadius: 50,
+        backgroundColor: "#f6f6f6",
+        height: 40,
+        margin: 12,
+        paddingLeft: 10,
+    },
+    textButtonCalculator: {
+        fontSize: 20,
+        color: "#ffffff",
+    },
+    buttonCalculator: {
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        width: "90%",
+        backgroundColor: "#ff0043",
+        paddingTop: 14,
+        paddingBottom: 14,
+        marginLeft: 20,
+        marginTop: 30,
+    },
+    errorMessage: {
+        fontSize: 12,
+        color: "red",
+        fontWeight: "bold",
+        paddingLeft: 20,
+    },
+    exibitionResultImc: {
+        width: "100%",
+        height: "50%",
+    },
+    listImcs: {
+        marginTop: 20,
+    },
+    resultImcItem: {
+        fontSize: 26,
+        color: "red",
+        height: 50,
+        width: "100%",
+        paddingRight: 20
+    },
+    textResultItemList: {
+        color: "red",
+        fontSize: 16,
+    }
+});
+
+export default styles
